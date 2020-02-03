0cdffcd4ba66cacb2fd857ba7626fdbcfc0d6fe3
Siarhey <siarhey.uchukhlebau@gmail.com>
2020-02-03 14:29:30 +0300

    Adds products name column (concatenated products name)

--- Plugin/AddDataToOrdersGrid.php
+++ Plugin/AddDataToOrdersGrid.php
@@ -6,6 +6,8 @@
 
 namespace MageWorx\ExtendedOrdersGrid\Plugin;
 
+use Magento\Sales\Model\ResourceModel\Order\Grid\Collection as OrderGridCollection;
+
 /**
  * Class AddDataToOrdersGrid
  */
@@ -31,7 +33,7 @@ class AddDataToOrdersGrid
 
     /**
      * @param \Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory $subject
-     * @param \Magento\Sales\Model\ResourceModel\Order\Grid\Collection $collection
+     * @param OrderGridCollection $collection
      * @param $requestName
      * @return mixed
      */
@@ -55,11 +57,47 @@ class AddDataToOrdersGrid
                     'soat.region_id = dcrt.region_id',
                     ['code']
                 );
+
+                // Add product's name column
+                $this->addProductsNameColumn($collection);
             } catch (\Zend_Db_Select_Exception $selectException) {
                 // Do nothing in that case
                 $this->logger->log(100, $selectException);
             }
         }
+
+        return $collection;
+    }
+
+    /**
+     * Adds products name column to the orders grid collection
+     *
+     * @param OrderGridCollection $collection
+     * @return OrderGridCollection
+     */
+    private function addProductsNameColumn(OrderGridCollection $collection): OrderGridCollection
+    {
+        // Get original table name
+        $orderItemsTableName = $collection->getResource()->getTable('sales_order_item');
+        // Create new select instance
+        $itemsTableSelectGrouped = $collection->getConnection()->select();
+        // Add table with columns which must be selected (skip useless columns)
+        $itemsTableSelectGrouped->from(
+            $orderItemsTableName,
+            [
+                'name'     => new \Zend_Db_Expr('GROUP_CONCAT(DISTINCT name SEPARATOR \',\')'),
+                'order_id' => 'order_id'
+            ]
+        );
+        // Group our select to make one column for one order
+        $itemsTableSelectGrouped->group('order_id');
+        // Add our sub-select to main collection with only one column: name
+        $collection->getSelect()
+                   ->joinLeft(
+                       ['soi' => $itemsTableSelectGrouped],
+                       'soi.order_id = main_table.entity_id',
+                       ['name']
+                   );
 
         return $collection;
     }
